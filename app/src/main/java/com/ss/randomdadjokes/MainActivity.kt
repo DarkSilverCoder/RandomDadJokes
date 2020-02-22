da86a9d79fcfe23b825dd22a9823b444edddcb7e
DarkSilverCoder <seanmtemple@yahoo.com>
2020-02-21 19:39:53 -0500

    Test

--- app/src/main/java/com/ss/randomdadjokes/MainActivity.kt
+++ app/src/main/java/com/ss/randomdadjokes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ss.randomdadjokes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         btnRDJ.setOnClickListener {
             // Code
             Log.i("MainActivity", "What did the fish say when it hit a wall? Dam.")
+            Toast.makeText(this, "What did the fish say when it hit a wall? Dam.", Toast.LENGTH_SHORT).show()
         }
 
     }
