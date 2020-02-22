1ec148c8860b7f5a3a7b4db692489ee555b5e4f2
DarkSilverCoder <seanmtemple@yahoo.com>
2020-02-21 19:18:53 -0500

    Test

--- app/src/main/java/com/ss/randomdadjokes/MainActivity.kt
+++ app/src/main/java/com/ss/randomdadjokes/MainActivity.kt
@@ -2,11 +2,20 @@ package com.ss.randomdadjokes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRDJ.setOnClickListener {
+            // Code
+            Log.i("MainActivity", "What did the fish say when it hit a wall? Dam.")
+        }
+
     }
+
 }
